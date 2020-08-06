4da5ca1a9a023b27118f518c14f7f7996781894d
goreev <goreev.ar@gmail.com>
2020-08-06 21:57:05 +0300

    create item mutation

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fb903510c6a0244f3a89";
+/******/ 	var hotCurrentHash = "6738b5b4ea35771bdf2f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
